94d336662f86a3a1761fb89eb9313e849129cd94
harshithr <harshithr.pro@gmail.com>
2020-09-27 20:40:38 +0530

    Updates

---
+++ precache-manifest.806e0c066f7e2a2050085a6f49e6a590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1296b4d9d807fbf9d660280d57672d6",
+    "url": "/dashboard-ui/index.html"
+  },
+  {
+    "revision": "dbc6071d2f89f3439ac1",
+    "url": "/dashboard-ui/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "557053428c347fb71595",
+    "url": "/dashboard-ui/static/css/main.10e881e0.chunk.css"
+  },
+  {
+    "revision": "dbc6071d2f89f3439ac1",
+    "url": "/dashboard-ui/static/js/2.751b7579.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dashboard-ui/static/js/2.751b7579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557053428c347fb71595",
+    "url": "/dashboard-ui/static/js/main.72c4b9ac.chunk.js"
+  },
+  {
+    "revision": "3ff5bdd22252170a4e80",
+    "url": "/dashboard-ui/static/js/runtime-main.ed844de8.js"
+  }
+]);
